adf02a788f5c09bab74fc2eff00bc5765cc51d78
tong <php12345@163.com>
2015-08-11 13:10:36 +0800

    first commit

---
+++ springtest01/src/main/java/com/tong/springtest/TestServlet.java
@@ -0,0 +1,21 @@
+package com.tong.springtest;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by tong on 8/11/15.
+ */
+public class TestServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.getWriter().write("testServlet......");
+        response.getWriter().flush();
+    }
+}
---
+++ springtest01/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: tong
+  Date: 8/10/15
+  Time: 22:21
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title></title>
+  </head>
+  <body>
+index.jsp
+  </body>
+</html>
